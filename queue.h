b496495b26a0ad40f0e0c5c32af30f726f79769d
shellygr <shelly6.g@gmail.com>
2013-03-12 09:26:29 +0200

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,7 +1,16 @@
 
-
 struct queue {
-  void* head;
-  void* tail;
+  elem* head;
+  elem* tail;
 };
 typedef struct queue queue;
+
+struct elem {
+  void* content;
+  elem* next;
+};
+typedef struct elem elem;
+
+void destroy_elem(elem* elm);
+elem* init_elem(void* content)
+
