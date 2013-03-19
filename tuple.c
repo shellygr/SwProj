1feaed00ec4f024fd88817f1378774c0abbae714
dorrrr <midor1@walla.co.il>
2013-03-19 11:44:23 +0200

    tuple.c
    very important class

---
+++ tuple.c
@@ -0,0 +1,20 @@
+typedef struct tuple{
+  int size;  
+  int score;
+  int place;
+  int averageWithin;
+};
+
+int compare_to(const void *a,const void *b){
+  tuple *x=const (tuple *)a;
+  tuple *y=const (tuple *)b;
+  if((x->size)>(y->size)){
+      return 1;
+  if((x->size)<(y->size))
+    return -1;
+  if((x->score)>(y->score)){
+      return 1;
+  if((x->score)<(y->score))
+    return -1;
+  return (x->place)-(y->place);  
+}
